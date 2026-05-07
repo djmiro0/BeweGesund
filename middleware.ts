b88e7c4210f8f167295bfeb4315f142a718d69ec
dmirosavljevic <djm@sernet.de>
2026-05-07 14:15:01 +0200

    working, fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import createMiddleware from "next-intl/middleware";
 
 const intlMiddleware = createMiddleware({
   locales: ["en", "de"],
-  defaultLocale: "de",
+  defaultLocale: "en",
   localeDetection: false
 });
 
